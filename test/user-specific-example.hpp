f3ae4ab1affdafc4ca767a36ce5c37ed2bf6912a
Reed Krantz <programming@krantz.one>
2021-10-12 12:05:28 -0500

    added comments to user-specific-example.hpp

--- test/user-specific-example.hpp
+++ test/user-specific-example.hpp
@@ -7,6 +7,7 @@
 // SESSION TESTS
 //****************
 
+// The federated IDs of all the Nextcloud accounts stored in Gnomes Online Accounts.
 const std::string TEST_SESSION_FEDERATEDIDS[] = { "user@cloud.example.com", "user2@cloud.example2.com" };
 
 
@@ -16,9 +17,11 @@ const std::string TEST_SESSION_FEDERATEDIDS[] = { "user@cloud.example.com", "use
 // PASSWORD TESTS
 //*****************
 
+// Account this example password is on.
 #define TEST_PASSWORD_ACCOUNT "user@cloud.example.com"
 
+// The example password itself. This information will be used to verify the libraries data.
 #define TEST_PASSWORD_UUID "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 #define TEST_PASSWORD_LABEL "Password Label"
-#define TEST_PASSWORD_USERNAME "Username"
+#define TEST_PASSWORD_USERNAME "Password Username"
 #define TEST_PASSWORD_PASSWORD "password123"
